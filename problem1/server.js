537c6afd820b30b6289af42c45b0991e331c3436
Yashwanthkumaram <68020051+Yashwanthkumaram@users.noreply.github.com>
2024-07-06 11:32:21 +0530

    c2

--- problem1/server.js
+++ problem1/server.js
@@ -2,7 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 
-app.listen(port, () => {
-    
+app.post('/average', (req, res) => {
+    const numbers = req.body;
+
+    if (!Array.isArray(numbers) || numbers.some(isNaN)) {
+        return res.status(400);
+    }
+
+    if (numbers.length === 0) {
+        return res.status(400);
+    }
+
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    const average = sum / numbers.length;
+
+    res.json({ average });
 });
+
+app.listen(port);
